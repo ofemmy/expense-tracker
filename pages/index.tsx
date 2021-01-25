13730725824ed2b671e7dcfb26a48a2971b36d17
Oladayo Fagbemi <oofagbemi@outlook.com>
2021-01-25 02:41:46 +0100

    initial setup

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Expense Tracker Application</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+        <h1>Help here</h1>
+      
+    </div>
+  )
+}
